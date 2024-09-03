e3420b0a8b028f15459f9372d66727ef9ebe623c
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-09-03 19:34:34 +0000

    SumOfThree

--- main.go
+++ main.go
@@ -10,13 +10,34 @@ import (
 func main() {
 
 	// Is Palindrome START
-	str := []string{"RACEACAR", "A", "ABCDEFGFEDCBA", "ABC", "ABCBA", "ABBA", "RACEACAR"}
-	for i, s := range str {
-		fmt.Printf("Test Case # %d\n", i+1)
-		fmt.Printf("%s\n", strings.Repeat("-", 100))
-		fmt.Printf("The input string is '%s' and the length of the string is %d.\n", s, len(s))
-		fmt.Printf("\nIs it a palindrome?.....%v\n", twopointers.IsPalindrome(s))
+	/* 	str := []string{"RACEACAR", "A", "ABCDEFGFEDCBA", "ABC", "ABCBA", "ABBA", "RACEACAR"}
+	   	for i, s := range str {
+	   		fmt.Printf("Test Case # %d\n", i+1)
+	   		fmt.Printf("%s\n", strings.Repeat("-", 100))
+	   		fmt.Printf("The input string is '%s' and the length of the string is %d.\n", s, len(s))
+	   		fmt.Printf("\nIs it a palindrome?.....%v\n", twopointers.IsPalindrome(s))
+	   		fmt.Printf("%s\n", strings.Repeat("-", 100))
+	   	} */
+	// Is Palindrome END
+
+	// SumOfThree START
+	numsLists := [][]int{
+		{3, 7, 1, 2, 8, 4, 5},
+		{-1, 2, 1, -4, 5, -3},
+		{2, 3, 4, 1, 7, 9},
+		{1, -1, 0},
+		{2, 4, 2, 7, 6, 3, 1},
+	}
+	tList := []int{10, 7, 20, -1, 8}
+	for i, nList := range numsLists {
+		fmt.Printf("%d. Input array: %s\n", i+1, strings.Replace(fmt.Sprint(nList), " ", ", ", -1))
+
+		if twopointers.FindSumOfThree(nList, tList[i]) {
+			fmt.Printf("   Sum for %d exists\n", tList[i])
+		} else {
+			fmt.Printf("   Sum for %d does not exist\n", tList[i])
+		}
 		fmt.Printf("%s\n", strings.Repeat("-", 100))
 	}
-	// Is Palindrome END
+	// SumOfThree END
 }
--- two-pointers/sum_of_three.go
+++ two-pointers/sum_of_three.go
@@ -1,1 +1,29 @@
 package twopointers
+
+import "sort"
+
+func FindSumOfThree(nums []int, target int) bool {
+	sort.Ints(nums)
+
+	var low int
+	var high int
+	var triple int
+
+	for i := 0; i < len(nums)-2; i++ {
+		low = i + 1
+		high = len(nums) - 1
+
+		for low < high {
+			triple = nums[1] + nums[low] + nums[high]
+
+			if triple == target {
+				return true
+			} else if triple > target {
+				high -= 1
+			} else {
+				low += 1
+			}
+		}
+	}
+	return false
+}
