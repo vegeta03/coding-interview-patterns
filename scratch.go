9cef6eae168c0e83d7030c4d06ae8789a629c3cf
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-09-09 05:37:45 +0000

    Slice

---
+++ scratch.go
@@ -0,0 +1,12 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import "fmt"
+
+func main() {
+	// Creating a int Slice
+	numbers := []int{1, 2, 3, 5, 6}
+	fmt.Printf("Slice: %[1]v\n", numbers)
+}
