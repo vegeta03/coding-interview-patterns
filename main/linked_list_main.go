a714a2aff859c347202e5f7cbca45ad69b6f7bdc
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-09-24 11:40:42 +0000

    linkedlist

---
+++ main/linked_list_main.go
@@ -0,0 +1,12 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import "github.com/vegeta03/coding-interview-patterns/linkedlist"
+
+func main() {
+	arr := []int{1, 2, 3, 4}
+	linkedList := linkedlist.CreateLinkedList(arr)
+	linkedList.PrintList()
+}
